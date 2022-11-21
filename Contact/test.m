12892aca75c3ae5d7a2ab133f4994e89dab82d9b
Lars-DHondt-KUL <lars.dhondt@student.kuleuven.be>
2022-11-21 08:01:20 +0100

    test volumetric contact ellipsoid

--- Contact/test.m
+++ Contact/test.m
@@ -9,7 +9,7 @@ vel = SX.sym('vel',1);
 
 radius = 0.032;
 
-stiffness = 60e6;
+stiffness = 10e6;
 dissipation = 2;
 transitionVelocity = 0.2;
 staticFriction = 0.8;
@@ -34,6 +34,42 @@ forceSX = HCContactModel(stiffness,radius,dissipation,...
 f_HC = Function('f_HC',{pos,vel},{forceSX(2)});
 
 
+%%
+
+pos = SX.sym('pos',1);
+vel = SX.sym('vel',1);
+
+radius = 0.032;
+
+stiffness2 = 16e6;
+dissipation2 = 0.565;
+normal  = [0;1;0];
+
+[force] = Brown_McPhee_contactSphere(stiffness2,radius,dissipation2,...
+    normal,[0;pos;0],[0;vel;0]);
+
+f_BM = Function('f_BM',{pos,vel},{force(2)});
+
+%%
+
+pos = SX.sym('pos',1);
+vel = SX.sym('vel',1);
+
+radius_E = [35.4, 54.0, 22.6]'*1e-3;
+sphereRot_inG = [0.329,0.201,0.101];
+
+
+stiffness = 8e7;
+dissipation = 0.565;
+normalvec  = [0;0;1];
+
+spherePos_inG = [0;0;pos];
+sphereLinVel_inG = [0;0;vel];
+
+[force,dist,disp,vol] = Brown_McPhee_contactEllipsoid(stiffness,dissipation,...
+    normalvec,radius_E,sphereRot_inG,spherePos_inG,sphereLinVel_inG);
+
+f_BM_E = Function('f_BM_E',{pos,vel},{force(3),dist,disp,vol});
 
 
 %%
@@ -43,46 +79,76 @@ f = 2.2;
 ampl = 4e-3; % m
 
 t = SX.sym('t',1);
+rds = SX.sym('radius',1);
+
 compr = -( cos(t*f*(2*pi)) - 1 )*ampl/2;
-y = radius - compr;
+y = rds - compr;
 ydot = jacobian(y,t);
 
-f_get_y_ydot = Function('f_get_y_ydot',{t},{y,ydot,compr});
+f_get_y_ydot = Function('f_get_y_ydot',{t,rds},{y,ydot,compr});
 
 
 %%
 N = 500;
 time = linspace(0,1/f,N);
 
-[pos,vel,cmpr] = f_get_y_ydot(time);
+[pos,vel,cmpr] = f_get_y_ydot(time,radius);
 
 load = f_HC(pos,vel);
 
+load2 = f_BM(pos,vel);
+
+[pos3,vel3,cmpr3] = f_get_y_ydot(time,28e-3);
+[load3,dist3,disp3,vol3] = f_BM_E(pos3,vel3);
+
+
 pos = full(pos);
+pos3 = full(pos3);
 vel = full(vel);
+vel3 = full(vel3);
 cmpr = full(cmpr);
+cmpr3 = full(cmpr3);
 load = full(load);
+load2 = full(load2);
+load3 = full(load3);
+dist3 = full(dist3);
+disp3 = full(disp3);
+vol3 = full(vol3);
 
 W_loading = trapz(cmpr(1:N/2),load(1:N/2));
 W_unloading = trapz(cmpr(N/2+1:end),load(N/2+1:end));
 W_total = trapz(cmpr,load);
 W_diss = W_total/W_loading*100
 
+W_loading3 = trapz(cmpr3(1:N/2),load3(1:N/2));
+W_total3 = trapz(cmpr3,load3);
+W_diss3 = W_total3/W_loading3*100
 
-% figure
-% tiledlayout("flow")
-% nexttile
-% plot(time,cmpr)
-% nexttile
-% plot(time,pos)
-% nexttile
-% plot(time,vel)
-% nexttile
-% plot(time,load)
+figure(2)
+tiledlayout("flow")
+nexttile
+plot(time,cmpr3)
+nexttile
+plot(time,pos3)
+nexttile
+plot(time,vel3)
+nexttile
+plot(time(1:N/2),load3(1:N/2))
+hold on
+plot(time(N/2+1:end),load3(N/2+1:end))
+nexttile
+plot(time,dist3)
+nexttile
+plot(time,disp3)
+nexttile
+plot(time,vol3)
+
 
 figure(1)
 hold on
 plot(cmpr*1e3,load*1e-3);
+plot(cmpr*1e3,load2*1e-3);
+plot(cmpr3*1e3,load3*1e-3);
 xlabel('Compression (mm)')
 ylabel('load (kN)')
 
